d952cf2f467abe5b04344fb52fb25c2e84cf33e8
aviel.berezin <aviel.berezin@gmail.com>
2024-03-01 23:05:00 +0200

    refactored run and cleaned pom

--- game-runner/run.bat
+++ game-runner/run.bat
@@ -1,16 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
 @echo off
 
-
-
-C:\Users\aviel\.jdks\openjdk-20.0.1\bin\java.exe -cp .\game\target\dependency\*;.\game\target\game-1.0-SNAPSHOT.jar rpd.game.SinglePlay %*
+mvn -q -f .\game-runner\pom.xml exec:java
